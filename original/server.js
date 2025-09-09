76aba7b81d47410eae2fe167739f95aa8d3d4c4c
Даниил <31047346+big123dan@users.noreply.github.com>
2025-09-09 07:54:36 +0000

    удалил лишнее

--- original/server.js
+++ original/server.js
@@ -11,12 +11,11 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Оригинальная форма оплаты
 app.post("/api/payment", (req, res) => {
-    console.log("💳 Оригинальный сайт получил:", req.body);
+    console.log("Оригинальный сайт получил -", req.body);
     res.json({ status: "success", message: "Оплата обработана" });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🏛️ Оригинальный сайт → http://localhost:8080`);
+    console.log(`Оригинальный сайт - http://localhost:8080`);
 });
